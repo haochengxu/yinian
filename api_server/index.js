535039570d782a394aff8bd4936df4f20f3ac255
徐浩程 <haochengx@gmail.com>
2018-04-22 11:59:30 +0800

    https

--- api_server/index.js
+++ api_server/index.js
@@ -1,10 +1,19 @@
 const path = require('path')
 var express = require('express');
 var app = express();
+var http = require('http')
+var https = require('https')
 var indexRouter = require('./routes');
+var fs = require('fs')
 
+var options = {
+    key: fs.readFileSync('../2_www.yinianxingzhe.club.key'),
+    cert: fs.readFileSync('../1_www.yinianxingzhe.club_bundle.crt')
+}
 app.use('/', indexRouter);
 
-app.listen(80, function() {
-    console.log("node listening at 80")
-});
+http.createServer(app).listen(80)
+https.createServer(options, app).listen(443)
+
+console.log("node listening at 80")
+console.log("node listening at 443")
